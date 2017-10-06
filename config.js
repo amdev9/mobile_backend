4370515c5ff0ce3a18640eff7e8957cc8c946c04
Alexander Matveev <blackkorol@gmail.com>
2017-10-07 00:02:35 +0300

    before deploy

---
+++ config.js
@@ -0,0 +1,18 @@
+
+const IP_ADDRESS = '51.15.66.253';
+const PORT = '3000';
+const URL = `http://${IP_ADDRESS}:${PORT}`;
+
+export const facebook = {
+  clientID: '806797486157972',
+  clientSecret: 'd52b0bf85fdbfdd57073d27afcf28fcf',
+  callbackURL: `${URL}/auth/facebook/callback`,
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const vkontakte = {
+  clientID: '6116024',
+  clientSecret: 'ytJDkCm5PFg0ZFQqO1LR',
+  callbackURL: `${URL}/auth/vkontakte/callback`,
+  // scope: ['status', 'email', 'friends', 'notify'] 
+};
